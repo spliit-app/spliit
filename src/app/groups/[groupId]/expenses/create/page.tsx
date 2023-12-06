fee1963284eafa6db7887103a156799b7610e13d
Sebastien Castiel <sebastien@castiel.me>
2023-12-06 13:01:57 -0500

    Remove button

--- src/app/groups/[groupId]/expenses/create/page.tsx
+++ src/app/groups/[groupId]/expenses/create/page.tsx
@@ -1,9 +1,6 @@
 import { ExpenseForm } from '@/components/expense-form'
-import { Button } from '@/components/ui/button'
 import { createExpense, getGroup } from '@/lib/api'
 import { expenseFormSchema } from '@/lib/schemas'
-import { ChevronLeft } from 'lucide-react'
-import Link from 'next/link'
 import { notFound, redirect } from 'next/navigation'
 
 export default async function ExpensePage({
@@ -21,17 +18,5 @@ export default async function ExpensePage({
     redirect(`/groups/${groupId}`)
   }
 
-  return (
-    <main>
-      <div className="mb-4">
-        <Button variant="ghost" asChild>
-          <Link href={`/groups/${groupId}`}>
-            <ChevronLeft className="w-4 h-4 mr-2" /> Back to group
-          </Link>
-        </Button>
-      </div>
-
-      <ExpenseForm group={group} onSubmit={createExpenseAction} />
-    </main>
-  )
+  return <ExpenseForm group={group} onSubmit={createExpenseAction} />
 }
