66ab0ff82bbcb3294d3a0fb84ae5ffcae9910e26
Sebastien Castiel <sebastien@castiel.me>
2023-12-15 10:34:00 -0500

    Add background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function HomePage() {
     <main>
       <section className="py-16 md:py-24 lg:py-32">
         <div className="container flex max-w-screen-md flex-col items-center gap-4 text-center">
-          <h1 className="!leading-none font-bold text-3xl sm:text-5xl md:text-6xl lg:text-7xl landing-header">
+          <h1 className="!leading-none font-bold text-3xl sm:text-5xl md:text-6xl lg:text-7xl landing-header py-2">
             Share <strong>Expenses</strong> <br /> with <strong>Friends</strong>{' '}
             & <strong>Family</strong>
           </h1>
