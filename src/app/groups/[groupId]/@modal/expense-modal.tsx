ceb6a7d7070050f95633984508987a6abc7df3b8
Sebastien Castiel <sebastien@castiel.me>
2023-12-15 13:05:52 -0500

    Use Vaul on small screens

--- src/app/groups/[groupId]/@modal/expense-modal.tsx
+++ src/app/groups/[groupId]/@modal/expense-modal.tsx
@@ -6,26 +6,28 @@ import {
   DialogTitle,
 } from '@/components/ui/dialog'
 import { useRouter } from 'next/navigation'
-import { ReactNode, useState } from 'react'
+import { ReactNode, useEffect, useState } from 'react'
+import { Drawer } from 'vaul'
 
-export function ExpenseModal({
-  children,
-  title,
-}: {
+type Props = {
   children: ReactNode
   title: ReactNode
-}) {
+}
+
+export function ExpenseModal(props: Props) {
+  const size = useTailwindBreakpoint()
+  if (size === 'xs') {
+    return <ExpenseVaul {...props} />
+  } else {
+    return <ExpenseDialog {...props} />
+  }
+}
+
+export function ExpenseDialog({ children, title }: Props) {
   const router = useRouter()
-  const [open, setOpen] = useState(true)
 
   return (
-    <Dialog
-      open={open}
-      onOpenChange={(open) => {
-        setOpen(open)
-        if (!open) router.back()
-      }}
-    >
+    <Dialog open onOpenChange={() => router.back()}>
       <DialogContent className="w-full max-w-screen-sm">
         <DialogHeader>
           <DialogTitle>{title}</DialogTitle>
@@ -34,4 +36,50 @@ export function ExpenseModal({
       </DialogContent>
     </Dialog>
   )
+}
+
+export function ExpenseVaul({ children, title }: Props) {
+  const router = useRouter()
+  return (
+    <Drawer.Root open onClose={() => router.back()}>
+      <Drawer.Portal>
+        <Drawer.Title>{title}</Drawer.Title>
+        <Drawer.Overlay
+          className="fixed inset-0 bg-background/80 backdrop-blur-sm"
+          onClick={() => router.back()}
+        />
+        <Drawer.Content className="bg-background border flex flex-col rounded-t-[10px] h-fit mt-24 fixed bottom-0 left-0 right-0 p-4 z-50">
+          <div className="text-xl font-bold mb-4">{title}</div>
+          {children}
+        </Drawer.Content>
+      </Drawer.Portal>
+    </Drawer.Root>
+  )
+}
+
+export function useTailwindBreakpoint() {
+  const [size, setSize] = useState<'xs' | 'sm' | 'md' | 'lg'>('xs')
+
+  useEffect(() => {
+    const handleBreakpointChange = () => {
+      if (window.innerWidth >= 1200) {
+        setSize('lg')
+      } else if (window.innerWidth >= 768) {
+        setSize('md')
+      } else if (window.innerWidth >= 640) {
+        setSize('sm')
+      } else {
+        setSize('xs')
+      }
+    }
+
+    window.addEventListener('resize', handleBreakpointChange)
+    handleBreakpointChange()
+
+    return () => {
+      window.removeEventListener('resize', handleBreakpointChange)
+    }
+  }, [])
+
+  return size
 }
