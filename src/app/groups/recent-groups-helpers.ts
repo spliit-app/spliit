55883ce414f5358a75780fbffb755f5464e211d4
Sebastien Castiel <sebastien@castiel.me>
2024-01-08 15:49:07 -0500

    Mark a group as favorite (fixes #29)

--- src/app/groups/recent-groups-helpers.ts
+++ src/app/groups/recent-groups-helpers.ts
@@ -7,10 +7,13 @@ export const recentGroupsSchema = z.array(
   }),
 )
 
+export const starredGroupsSchema = z.array(z.string())
+
 export type RecentGroups = z.infer<typeof recentGroupsSchema>
 export type RecentGroup = RecentGroups[number]
 
 const STORAGE_KEY = 'recentGroups'
+const STARRED_GROUPS_STORAGE_KEY = 'starredGroups'
 
 export function getRecentGroups() {
   const groupsInStorageJson = localStorage.getItem(STORAGE_KEY)
@@ -26,5 +29,30 @@ export function saveRecentGroup(group: RecentGroup) {
   localStorage.setItem(
     STORAGE_KEY,
     JSON.stringify([group, ...recentGroups.filter((rg) => rg.id !== group.id)]),
+  )
+}
+
+export function getStarredGroups() {
+  const starredGroupsJson = localStorage.getItem(STARRED_GROUPS_STORAGE_KEY)
+  const starredGroupsRaw = starredGroupsJson
+    ? JSON.parse(starredGroupsJson)
+    : []
+  const parseResult = starredGroupsSchema.safeParse(starredGroupsRaw)
+  return parseResult.success ? parseResult.data : []
+}
+
+export function starGroup(groupId: string) {
+  const starredGroups = getStarredGroups()
+  localStorage.setItem(
+    STARRED_GROUPS_STORAGE_KEY,
+    JSON.stringify([...starredGroups, groupId]),
+  )
+}
+
+export function unstarGroup(groupId: string) {
+  const starredGroups = getStarredGroups()
+  localStorage.setItem(
+    STARRED_GROUPS_STORAGE_KEY,
+    JSON.stringify(starredGroups.filter((g) => g !== groupId)),
   )
 }
