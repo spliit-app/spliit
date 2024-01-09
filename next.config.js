4c83490f38e3d9b70241624e1567067342439a3c
Sebastien Castiel <sebastien@castiel.me>
2024-01-09 11:25:42 -0500

    Add contributors on home page

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [{ hostname: 'avatars.githubusercontent.com' }],
+  },
+}
 
 const { withPlausibleProxy } = require('next-plausible')
 module.exports = withPlausibleProxy()(nextConfig)
