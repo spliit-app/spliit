7376a964017314638185ccbc70ad77ecf970220f
Lauri Vuorela <lauri.vuorela@gmail.com>
2024-01-25 12:01:16 +0100

    use dummy data instead

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-if [ -f container.env.example ]; then
-    set -a
-    source container.env.example
-    set +a
-else
-    echo "container.env.example needed to build"
-    exit
-fi
-
 SPLIIT_APP_NAME=$(node -p -e "require('./package.json').name")
 SPLIIT_VERSION=$(node -p -e "require('./package.json').version")
 
@@ -18,10 +9,11 @@ else
     TARGET=production
 fi
 
+# we need to set dummy data for POSTGRES env vars in order for build not to fail
 docker buildx build \
     --no-cache \
-    --build-arg POSTGRES_PRISMA_URL\
-    --build-arg POSTGRES_URL_NON_POOLING\
+    --build-arg POSTGRES_PRISMA_URL=postgresql://build:@db \
+    --build-arg POSTGRES_URL_NON_POOLING=postgresql://build:@db \
     -t ${SPLIIT_APP_NAME}:${SPLIIT_VERSION} \
     -t ${SPLIIT_APP_NAME}:latest \
     --target ${TARGET} \
