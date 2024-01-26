11d2e298e8cc5cac07aa2c60e194bf4460cefb21
Sebastien Castiel <sebastien@castiel.me>
2024-01-26 16:11:51 -0500

    Improve recent groups design

--- src/app/groups/recent-group-list.tsx
+++ src/app/groups/recent-group-list.tsx
@@ -155,7 +155,7 @@ function GroupList({
   setState: (state: SetStateAction<RecentGroupsState>) => void
 }) {
   return (
-    <ul className="grid grid-cols-1 gap-2 sm:grid-cols-3">
+    <ul className="grid gap-2 sm:grid-cols-2">
       {groups.map((group) => (
         <RecentGroupListCard
           key={group.id}
