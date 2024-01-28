f9e164f0f9d187adc061fd4ecd14b941f1f6a88e
Sebastien Castiel <sebastien@castiel.me>
2024-01-28 18:48:46 -0500

    Fix file name issue

--- src/app/api/s3-upload/route.ts
+++ src/app/api/s3-upload/route.ts
@@ -1,1 +1,8 @@
-export { POST } from 'next-s3-upload/route'
+import { sanitizeKey } from 'next-s3-upload'
+import { POST as route } from 'next-s3-upload/route'
+
+export const POST = route.configure({
+  key(req, filename) {
+    return sanitizeKey(filename).toLowerCase()
+  },
+})
