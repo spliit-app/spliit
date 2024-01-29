7695ffd62d2f6a2aa786270945919904ee067185
Sebastien Castiel <sebastien@castiel.me>
2024-01-29 10:39:49 -0500

    Fix uploaded image names

--- src/app/api/s3-upload/route.ts
+++ src/app/api/s3-upload/route.ts
@@ -1,8 +1,11 @@
-import { sanitizeKey } from 'next-s3-upload'
+import { randomId } from '@/lib/api'
 import { POST as route } from 'next-s3-upload/route'
 
 export const POST = route.configure({
   key(req, filename) {
-    return sanitizeKey(filename).toLowerCase()
+    const [, extension] = filename.match(/(\.[^\.]*)$/) ?? [null, '']
+    const timestamp = new Date().toISOString()
+    const random = randomId()
+    return `document-${timestamp}-${random}${extension.toLowerCase()}`
   },
 })
