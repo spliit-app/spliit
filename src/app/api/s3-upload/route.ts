219f4ac1299482133806c0c6bf334e834d40dc37
mertd <mertd@users.noreply.github.com>
2024-01-31 19:42:07 +0900

    use type safe env

--- src/app/api/s3-upload/route.ts
+++ src/app/api/s3-upload/route.ts
@@ -1,11 +1,12 @@
 import { sanitizeKey } from 'next-s3-upload'
 import { POST as route } from 'next-s3-upload/route'
+import { env } from '@/lib/env'
 
 export const POST = route.configure({
   key(req, filename) {
     return sanitizeKey(filename).toLowerCase()
   },
-  endpoint: process.env.S3_UPLOAD_ENDPOINT,
+  endpoint: env.S3_UPLOAD_ENDPOINT,
   // forcing path style is only necessary for providers other than AWS
-  forcePathStyle: !!process.env.S3_UPLOAD_ENDPOINT,
+  forcePathStyle: !!env.S3_UPLOAD_ENDPOINT,
 })
