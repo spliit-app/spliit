10089dffc18fe9c86713e4705c26adabb482c169
Stefan Hynst <stefan@hynst.at>
2024-02-10 16:56:17 +0100

    Fix: parameters could be defined outside .env

--- scripts/generate-prisma-schema.sh
+++ scripts/generate-prisma-schema.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-ENV_FILE=".env"
 PRISMA_SCHEMA="./prisma/schema.prisma"
 PRISMA_SCHEMA_TEMPLATE="${PRISMA_SCHEMA}.template"
 
-if [ ! -f "$ENV_FILE" ]; then
-    echo "ERROR: Environment variables not defined, file ${ENV_FILE} is missing!"
-    exit 1
+if [ -f ".env" ]; then
+    source .env
 fi
 
 if [ ! -f "$PRISMA_SCHEMA_TEMPLATE" ]; then
@@ -14,11 +12,10 @@ if [ ! -f "$PRISMA_SCHEMA_TEMPLATE" ]; then
     exit 1
 fi
 
-source "$ENV_FILE"
 PROVIDER=$( echo "$PRISMA_URL" | cut -d ':' -f 1 )
 
 if [ -z "$PROVIDER" ]; then
-    echo "Environment variable PRISMA_URL not defined in ${ENV_FILE}!"
+    echo "Environment variable PRISMA_URL not defined!"
 else
     echo "Generating prisma schema for provider \"${PROVIDER}\""
 
