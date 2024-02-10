d35acefa0480de3f6ee74d621e6f2f1ec154a4e2
Stefan Hynst <stefan@hynst.at>
2024-02-10 15:55:25 +0100

    Fix lint warning about missing effect dependency

--- src/components/category-selector.tsx
+++ src/components/category-selector.tsx
@@ -41,7 +41,7 @@ export function CategorySelector({
   useEffect(() => {
     setValue(defaultValue)
     onValueChange(defaultValue)
-  }, [defaultValue])
+  }, [defaultValue, onValueChange])
 
   const selectedCategory =
     categories.find((category) => category.id === value) ?? categories[0]
