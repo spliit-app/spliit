2a4e769096297f8a3bf7640164d0809cd12f560c
Stefan Hynst <stefan@hynst.at>
2024-02-18 17:29:01 +0100

    Implement infinite scroll
    - in server component Page
      - only load first 200 expenses max
      - pass preloaded expenses and total count
    
    - in client component ExpenseList, if there are more expenses to show
      - test if there are more expenses
      - append preloading "skeletons" to end of list
      - fetch more expenses when last item in list comes into view
      - after each fetch increase fetch-length by factor 1.5
        - rationale: db fetch usually is not the issue here, the longer the list gets, the longer react needs to redraw

--- src/app/groups/[groupId]/expenses/expense-list.tsx
+++ src/app/groups/[groupId]/expenses/expense-list.tsx
@@ -2,16 +2,19 @@
 import { ExpenseCard } from '@/app/groups/[groupId]/expenses/expense-card'
 import { Button } from '@/components/ui/button'
 import { SearchBar } from '@/components/ui/search-bar'
+import { Skeleton } from '@/components/ui/skeleton'
 import { getGroupExpenses } from '@/lib/api'
 import { Participant } from '@prisma/client'
 import dayjs, { type Dayjs } from 'dayjs'
 import Link from 'next/link'
 import { useEffect, useMemo, useState } from 'react'
+import { useInView } from 'react-intersection-observer'
 
 type ExpensesType = Awaited<ReturnType<typeof getGroupExpenses>>
 
 type Props = {
-  expenses: ExpensesType
+  expensesFirstPage: ExpensesType
+  expenseCount: number
   participants: Participant[]
   currency: string
   groupId: string
@@ -52,13 +55,41 @@ function getGroupedExpensesByDate(expenses: ExpensesType) {
   }, {})
 }
 
+async function fetchExpenses(
+  groupId: string,
+  offset: number,
+  length: number,
+): Promise<ExpensesType> {
+  const resp = await fetch(
+    `/api/groups/${groupId}/expenses?offset=${offset}&length=${length}`,
+  )
+
+  const expenses = resp.ok && ((await resp.json()) as ExpensesType)
+  if (expenses === false) throw 'internal error'
+
+  expenses.forEach((e) => {
+    e.createdAt = new Date(e.createdAt)
+    e.expenseDate = new Date(e.expenseDate)
+  })
+  return expenses
+}
+
 export function ExpenseList({
-  expenses,
+  expensesFirstPage,
+  expenseCount,
   currency,
   participants,
   groupId,
 }: Props) {
+  const firstLen = expensesFirstPage.length
   const [searchText, setSearchText] = useState('')
+  const [dataIndex, setDataIndex] = useState(firstLen)
+  const [dataLen, setDataLen] = useState(firstLen)
+  const [hasMoreData, setHasMoreData] = useState(expenseCount > firstLen)
+  const [isFetching, setIsFetching] = useState(false)
+  const [expenses, setExpenses] = useState(expensesFirstPage)
+  const { ref, inView } = useInView()
+
   useEffect(() => {
     const activeUser = localStorage.getItem('newGroup-activeUser')
     const newUser = localStorage.getItem(`${groupId}-newUser`)
@@ -77,6 +108,35 @@ export function ExpenseList({
       }
     }
   }, [groupId, participants])
+
+  useEffect(() => {
+    const fetchNextPage = async () => {
+      setIsFetching(true)
+
+      try {
+        const newExpenses = expenses.concat(
+          await fetchExpenses(groupId, dataIndex, dataLen),
+        )
+        setExpenses(newExpenses)
+        setHasMoreData(newExpenses.length < expenseCount)
+        setDataIndex(dataIndex + dataLen)
+        setDataLen(Math.ceil(1.5 * dataLen))
+      } catch {}
+
+      setTimeout(() => setIsFetching(false), 500)
+    }
+
+    if (inView && hasMoreData && !isFetching) fetchNextPage()
+  }, [
+    dataIndex,
+    dataLen,
+    expenseCount,
+    expenses,
+    groupId,
+    hasMoreData,
+    inView,
+    isFetching,
+  ])
 
   const groupedExpensesByDate = useMemo(
     () => getGroupedExpensesByDate(expenses),
@@ -116,6 +176,22 @@ export function ExpenseList({
           </div>
         )
       })}
+      {expenses.length < expenseCount &&
+        [0, 1, 2].map((i) => (
+          <div
+            key={i}
+            className="border-t flex justify-between items-center px-6 py-4 text-sm"
+            ref={i === 0 ? ref : undefined}
+          >
+            <div className="flex flex-col gap-2">
+              <Skeleton className="h-4 w-16 rounded-full" />
+              <Skeleton className="h-4 w-32 rounded-full" />
+            </div>
+            <div>
+              <Skeleton className="h-4 w-16 rounded-full" />
+            </div>
+          </div>
+        ))}
     </>
   ) : (
     <p className="px-6 text-sm py-6">
