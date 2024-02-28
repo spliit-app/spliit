01973591625e2f7a79f6c0cfa22158f69c94e038
Alexander Zinov <sashkent3@gmail.com>
2024-02-28 19:52:32 +0400

    prettier

--- src/components/expense-form.tsx
+++ src/components/expense-form.tsx
@@ -103,7 +103,10 @@ export function ExpenseForm({
           paidBy: searchParams.get('from') ?? undefined,
           paidFor: [
             searchParams.get('to')
-              ? { participant: searchParams.get('to')!, shares: ('1' as unknown as number) }
+              ? {
+                  participant: searchParams.get('to')!,
+                  shares: '1' as unknown as number,
+                }
               : undefined,
           ],
           isReimbursement: true,
@@ -122,7 +125,7 @@ export function ExpenseForm({
           // paid for all, split evenly
           paidFor: group.participants.map(({ id }) => ({
             participant: id,
-            shares: ('1' as unknown as number),
+            shares: '1' as unknown as number,
           })),
           paidBy: getSelectedPayer(),
           isReimbursement: false,
