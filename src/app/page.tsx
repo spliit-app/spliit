18e7d2cab39b577b9f868fbca95145471735dca3
Diane <dianezhou96@gmail.com>
2024-03-02 21:13:33 -0800

    additional refinement to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,15 +11,19 @@ export default function HomePage() {
         <div className="container flex max-w-screen-md flex-col items-center gap-4 text-center">
           <h1 className="!leading-none font-bold text-3xl sm:text-5xl md:text-6xl lg:text-7xl landing-header py-2">
             Share <strong>Expenses</strong> <br /> with <strong>Friends</strong>{' '}
-            & <strong>Family</strong>
+            & <strong>Family</strong> for <strong>Free</strong>...{' '}
+            <strong>Unlimited</strong>!
           </h1>
           <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
-            Welcome to <strong>Splitdumb</strong>, not Splitwise!
+            Welcome to <strong>Splitdumb</strong>,
             <br />
+            because Splitwise is dumb and should be free!
           </p>
           <div className="flex gap-2">
             <Button asChild>
-              <Link href="/groups">Go to groups</Link>
+              <Link href="/groups">
+                <span className="md:text-lg">Go to groups</span>
+              </Link>
             </Button>
           </div>
         </div>
