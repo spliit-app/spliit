cc37083389de407e760abb4652b83cbdbc53bfa3
annalouisep <annalouisepercy@gmail.com>
2024-03-09 10:30:24 -0600

    Expense list: add section for planned purchases (#122)
    * add planned purchases
    
    * Updating verbiage to reflect possible future entry types

--- src/app/groups/[groupId]/expenses/expense-list.tsx
+++ src/app/groups/[groupId]/expenses/expense-list.tsx
@@ -19,6 +19,7 @@ type Props = {
 }
 
 const EXPENSE_GROUPS = {
+  UPCOMING: 'Upcoming',
   THIS_WEEK: 'This week',
   EARLIER_THIS_MONTH: 'Earlier this month',
   LAST_MONTH: 'Last month',
@@ -28,7 +29,9 @@ const EXPENSE_GROUPS = {
 }
 
 function getExpenseGroup(date: Dayjs, today: Dayjs) {
-  if (today.isSame(date, 'week')) {
+  if (today.isBefore(date)) {
+    return EXPENSE_GROUPS.UPCOMING
+  } else if (today.isSame(date, 'week')) {
     return EXPENSE_GROUPS.THIS_WEEK
   } else if (today.isSame(date, 'month')) {
     return EXPENSE_GROUPS.EARLIER_THIS_MONTH
