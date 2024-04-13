1cd2b273f9c6fed77aa1be9286c1a164c82bcca8
dcbr <15089458+dcbr@users.noreply.github.com>
2024-04-13 19:07:18 +0200

    Show the impact of an expense on the active user's balance (#139)
    * Add devcontainer configuration for codespace support
    
    * Show the impact of an expense on the active user's balance
    
    * Run prettier
    
    * Put the balance on a different line
    
    ---------
    
    Co-authored-by: Sebastien Castiel <sebastien@castiel.me>

--- src/app/groups/[groupId]/expenses/expense-list.tsx
+++ src/app/groups/[groupId]/expenses/expense-list.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { ActiveUserBalance } from '@/app/groups/[groupId]/expenses/active-user-balance'
 import { CategoryIcon } from '@/app/groups/[groupId]/expenses/category-icon'
 import { Button } from '@/components/ui/button'
 import { SearchBar } from '@/components/ui/search-bar'
@@ -150,6 +151,9 @@ export function ExpenseList({
                         </strong>
                       </Fragment>
                     ))}
+                  </div>
+                  <div className="text-xs text-muted-foreground">
+                    <ActiveUserBalance {...{ groupId, currency, expense }} />
                   </div>
                 </div>
                 <div className="flex flex-col justify-between items-end">
