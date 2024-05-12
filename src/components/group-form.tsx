e1619ec44f107d2165f59437e79626c76ee24608
Oliver Wong <oliver@owiber.com>
2024-05-11 21:30:37 -0700

    use placeholder for new participant

--- src/components/group-form.tsx
+++ src/components/group-form.tsx
@@ -173,7 +173,7 @@ export function GroupForm({
                         </FormLabel>
                         <FormControl>
                           <div className="flex gap-2">
-                            <Input className="text-base" {...field} />
+                            <Input className="text-base" {...field} placeholder="New"/>
                             {item.id &&
                             protectedParticipantIds.includes(item.id) ? (
                               <HoverCard>
@@ -221,7 +221,7 @@ export function GroupForm({
             <Button
               variant="secondary"
               onClick={() => {
-                append({ name: 'New' })
+                append({ name: '' })
               }}
               type="button"
             >
