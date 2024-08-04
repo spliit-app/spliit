b5cb32d5a8421beb0e885479e9651e9430d717e8
Andy Trouvé <andy@strekol.eu>
2024-08-04 09:01:04 +0200

    Revert back default language to en-US

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,7 +4,7 @@ import { getUserLocale } from './lib/locale'
 export const locales = ['en-US', 'fi', 'fr-FR'] as const
 export type Locale = (typeof locales)[number]
 export type Locales = ReadonlyArray<Locale>
-export const defaultLocale: Locale = 'fr-FR'
+export const defaultLocale: Locale = 'en-US'
 
 export default getRequestConfig(async () => {
   const locale = await getUserLocale()
