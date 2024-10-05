8cdc1494db854a775153368daa1515ae9844c533
Sebastien Castiel <sebastien@castiel.me>
2024-10-05 10:29:38 -0400

    Prettier

--- src/i18n.ts
+++ src/i18n.ts
@@ -9,7 +9,7 @@ export const locales = [
   'de-DE',
   'zh-CN',
   'ru-RU',
-  'it-IT'
+  'it-IT',
 ] as const
 export type Locale = (typeof locales)[number]
 export type Locales = ReadonlyArray<Locale>
