74c7b7606bf79d0966e2a0168f17e46c7a450abf
Sebastien Castiel <sebastien@castiel.me>
2024-10-19 19:47:18 -0400

    Use tRPC in group stats

--- src/trpc/routers/groups/index.ts
+++ src/trpc/routers/groups/index.ts
@@ -2,11 +2,13 @@ import { createTRPCRouter } from '@/trpc/init'
 import { groupBalancesRouter } from '@/trpc/routers/groups/balances'
 import { groupExpensesRouter } from '@/trpc/routers/groups/expenses'
 import { getGroupProcedure } from '@/trpc/routers/groups/get.procedure'
+import { groupStatsRouter } from '@/trpc/routers/groups/stats'
 import { updateGroupProcedure } from '@/trpc/routers/groups/update.procedure'
 
 export const groupsRouter = createTRPCRouter({
   expenses: groupExpensesRouter,
   balances: groupBalancesRouter,
+  stats: groupStatsRouter,
 
   get: getGroupProcedure,
   update: updateGroupProcedure,
