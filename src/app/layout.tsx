084bdd893ca4c32d6ae1b572beab0ade1b6ac35a
Rafael Macedo <rafaelmacedo4@gmail.com>
2025-10-27 23:44:10 +0000

    Remove footer (#2)
    * chore: remove the footer section from multiple language files
    
    * chore: update Next.js version to 14.2.33 and rename package to knots

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -105,49 +105,7 @@ function Content({ children }: { children: React.ReactNode }) {
             </ul>
           </div>
         </header>
-
         <div className="flex-1 flex flex-col">{children}</div>
-
-        <footer className="sm:p-8 md:p-16 sm:mt-16 sm:text-sm md:text-base md:mt-32 bg-slate-50 dark:bg-card border-t p-6 mt-8 flex flex-col sm:flex-row sm:justify-between gap-4 text-xs [&_a]:underline">
-          <div className="flex flex-col space-y-2">
-            <div className="sm:text-lg font-semibold text-base flex space-x-2 items-center">
-              <Link className="flex items-center gap-2" href="/">
-                <Image
-                  src="/logo-with-text.png"
-                  className="m-1 h-auto w-auto"
-                  width={(35 * 522) / 180}
-                  height={35}
-                  alt="Spliit"
-                />
-              </Link>
-            </div>
-            <div className="flex flex-col space-y a--no-underline-text-white">
-              <span>{t('Footer.madeIn')}</span>
-              <span>
-                {t.rich('Footer.builtBy', {
-                  author: (txt) => (
-                    <a
-                      href="https://scastiel.dev"
-                      target="_blank"
-                      rel="noopener"
-                    >
-                      {txt}
-                    </a>
-                  ),
-                  source: (txt) => (
-                    <a
-                      href="https://github.com/spliit-app/spliit/graphs/contributors"
-                      target="_blank"
-                      rel="noopener"
-                    >
-                      {txt}
-                    </a>
-                  ),
-                })}
-              </span>
-            </div>
-          </div>
-        </footer>
         <Toaster />
       </TooltipProvider>
     </TRPCProvider>
