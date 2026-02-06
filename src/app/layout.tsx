df75de1e7f76bee6317db7302a0b28e3186354bc
carnach <gevert@gmail.com>
2026-02-06 12:10:50 +0000

    update UI remove import button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,18 +47,27 @@ export const metadata: Metadata = {
     title: 'Spliit',
   },
   applicationName: 'Spliit',
-  icons: [
-    {
-      url: '/android-chrome-192x192.png',
-      sizes: '192x192',
-      type: 'image/png',
-    },
-    {
-      url: '/android-chrome-512x512.png',
-      sizes: '512x512',
-      type: 'image/png',
-    },
-  ],
+  icons: {
+    icon: [
+      {
+        url: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        url: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+    apple: [
+      {
+        url: '/logo/192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+    ],
+  },
 }
 
 export const viewport: Viewport = {
